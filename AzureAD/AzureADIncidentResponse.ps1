32a8bf6cbed5d001e3a3f42e488bfffd9b680453
Alex Verboon <verboonalex@gmail.com>
2021-04-06 19:17:08 +0200

    Create AzureADIncidentResponse.ps1

---
+++ AzureAD/AzureADIncidentResponse.ps1
@@ -0,0 +1,4 @@
+## https://www.powershellgallery.com/packages/AzureADIncidentResponse/4.0
+## Install AzureADIncident Response Module from MS DART TEam
+Install-Module -Name AzureADIncidentResponse -Scope CurrentUser
+Get-Command -Module AzureADIncidentResponse

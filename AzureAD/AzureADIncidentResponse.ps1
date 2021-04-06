a3801f597358ef7f5a68db3ad470a770592d2fb4
Alex Verboon <verboonalex@gmail.com>
2021-04-06 19:41:42 +0200

    Update AzureADIncidentResponse.ps1

--- AzureAD/AzureADIncidentResponse.ps1
+++ AzureAD/AzureADIncidentResponse.ps1
@@ -1,4 +1,15 @@
 ## https://www.powershellgallery.com/packages/AzureADIncidentResponse/4.0
-## Install AzureADIncident Response Module from MS DART TEam
+## Install AzureADIncident Response Module from MS DART Team
+
+
 Install-Module -Name AzureADIncidentResponse -Scope CurrentUser
 Get-Command -Module AzureADIncidentResponse
+
+$domainname = "verboon.online"
+$TenantID =   (Get-AzureADIRTenantId -DomainName $domainname)
+ 
+Connect-AzureADIR -TenantId $TenantID
+Get-AzureADIRConditionalAccessPolicy -All -TenantId $TenantID
+Get-AzureADIRMfaAuthMethodAnalysis -TenantId $TenantID 
+Get-AzureADIRUserLastSignInActivity -TenantId $TenantID -StaleThreshold 90 
+Get-AzureADIRPermission -TenantId $TenantID
